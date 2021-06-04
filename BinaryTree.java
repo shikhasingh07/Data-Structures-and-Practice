1ee1d4945968c04bc5a5ef546f6dcb827bd21aaa
golirocker <shikha.thakur2295@gmail.com>
2021-06-04 23:04:28 +0530

    Construct

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,4 +1,3 @@
-import java.io.*;
 import java.util.*;
 
 public class BinaryTree {
@@ -32,31 +31,24 @@ public class BinaryTree {
     public static Node construct(Integer[] arr) {
         Node root = new Node(arr[0]);
         Stack<Pair> st = new Stack<>();
-        Pair rp = new Pair(root, 1);
-        st.push(rp);
+        st.push(new Pair(root, 0));
         int index = 0;
         while (st.size() > 0) {
             Pair top = st.peek();
-            if (top.state == 1) {
+            if (top.state == 0) {
                 index++;
                 if (arr[index] != null) {
                     Node newNode = new Node(arr[index]);
                     top.node.left = newNode;
-                    Pair lp = new Pair(top.node.left, 1);
-                    st.push(lp);
-                } else {
-                    top.node.left = null;
+                    st.push(new Pair(newNode, 0));
                 }
                 top.state++;
-            } else if (top.state == 2) {
+            } else if (top.state == 1) {
                 index++;
                 if (arr[index] != null) {
                     Node newNode = new Node(arr[index]);
                     top.node.right = newNode;
-                    Pair Rp = new Pair(top.node.right, 2);
-                    st.push(Rp);
-                } else {
-                    top.node.right = null;
+                    st.push(new Pair(newNode, 0));
                 }
                 top.state++;
             } else {
@@ -66,10 +58,24 @@ public class BinaryTree {
         return root;
     }
 
+    public static void display(Node root) {
+        if (root == null)
+            return;
+
+        String str = root.left == null ? " ." : "" + root.left.data;
+        str += " <- [" + root.data + "] -> ";
+        str += root.right == null ? ". " : root.right.data;
+        System.out.println(str);
+
+        display(root.left);
+        display(root.right);
+    }
+
     public static void fun() {
         Integer[] arr = { 50, 25, 12, null, null, 37, 30, null, null, null, 75, 62, null, 70, null, null, 87, null,
                 null };
         Node root = construct(arr);
+        display(root);
     }
 
     public static void main(String[] args) {
