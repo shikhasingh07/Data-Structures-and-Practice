554927a1e029418c9f8113638c3e538e77877492
golirocker <shikha.thakur2295@gmail.com>
2021-06-04 22:37:43 +0530

    Construct

--- BinaryTree.java
+++ BinaryTree.java
@@ -2,9 +2,74 @@ import java.io.*;
 import java.util.*;
 
 public class BinaryTree {
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = this.right = null;
+        }
+
+        public Node(int data, Node left, Node right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static class Pair {
+        Node node;
+        int state;
+
+        Pair(Node node, int state) {
+            this.node = node;
+            this.state = state;
+        }
+    }
+
+    public static Node construct(Integer[] arr) {
+        Node root = new Node(arr[0]);
+        Stack<Pair> st = new Stack<>();
+        Pair rp = new Pair(root, 1);
+        st.push(rp);
+        int index = 0;
+        while (st.size() > 0) {
+            Pair top = st.peek();
+            if (top.state == 1) {
+                index++;
+                if (arr[index] != null) {
+                    Node newNode = new Node(arr[index]);
+                    top.node.left = newNode;
+                    Pair lp = new Pair(top.node.left, 1);
+                    st.push(lp);
+                } else {
+                    top.node.left = null;
+                }
+                top.state++;
+            } else if (top.state == 2) {
+                index++;
+                if (arr[index] != null) {
+                    Node newNode = new Node(arr[index]);
+                    top.node.right = newNode;
+                    Pair Rp = new Pair(top.node.right, 2);
+                    st.push(Rp);
+                } else {
+                    top.node.right = null;
+                }
+                top.state++;
+            } else {
+                st.pop();
+            }
+        }
+        return root;
+    }
 
     public static void fun() {
-        System.out.println("Binary Tree");
+        Integer[] arr = { 50, 25, 12, null, null, 37, 30, null, null, null, 75, 62, null, 70, null, null, 87, null,
+                null };
+        Node root = construct(arr);
     }
 
     public static void main(String[] args) {
