4f13b0d75fc33e906e6a5b2be4e4ad031572e488
golirocker <shikha.thakur2295@gmail.com>
2021-06-04 19:53:20 +0530

    BinaryTree

---
+++ BinaryTree.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class BinaryTree {
+
+    public static void fun() {
+        System.out.println("Binary Tree");
+    }
+
+    public static void main(String[] args) {
+        fun();
+    }
+}
