061128715c3be00ae1cc1d97703a3bcc392d9754
golirocker <shikha.thakur2295@gmail.com>
2021-06-06 02:22:57 +0530

    Update

--- BinaryTree.java
+++ BinaryTree.java
@@ -148,10 +148,107 @@ public class BinaryTree {
         }
     }
 
+    public static void iterativePrePostInTraversal(Node node) {
+        // write your code here
+        Stack<Pair> st = new Stack<>();
+        st.push(new Pair(node, 0));
+        ArrayList<Integer> pre = new ArrayList<>();
+        ArrayList<Integer> In = new ArrayList<>();
+        ArrayList<Integer> post = new ArrayList<>();
+
+        while (st.size() > 0) {
+            Pair p = st.peek();
+            if (p.state == 0) {
+                pre.add(p.node.data);
+                p.state++;
+                if (p.node.left != null) {
+                    st.push(new Pair(p.node.left, 0));
+                }
+            } else if (p.state == 1) {
+                In.add(p.node.data);
+                p.state++;
+                if (p.node.right != null) {
+                    st.push(new Pair(p.node.right, 0));
+                }
+            } else {
+                post.add(p.node.data);
+                st.pop();
+            }
+        }
+        for (int val : pre) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+        for (int val : In) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+        for (int val : post) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
+
+    public static boolean find(Node node, int find) {
+        if (node == null) {
+            return false;
+        }
+        if (find == node.data) {
+            return true;
+        }
+        boolean f1 = find(node.left, find);
+        if (f1) {
+            return true;
+        }
+        boolean f2 = find(node.right, find);
+        if (f2) {
+            return true;
+        }
+        return false;
+    }
+
+    public static ArrayList<Integer> nodeTopath(Node node, int fin) {
+        if (node == null) {
+            return new ArrayList<>();
+        }
+
+        if (node.data == fin) {
+            ArrayList<Integer> bres = new ArrayList<>();
+            bres.add(node.data);
+            return bres;
+        }
+
+        ArrayList<Integer> lres = nodeTopath(node.left, fin);
+        if (lres.size() > 0) {
+            lres.add(node.data);
+            return lres;
+        }
+
+        ArrayList<Integer> rres = nodeTopath(node.right, fin);
+        if (rres.size() > 0) {
+            rres.add(node.data);
+            return rres;
+        }
+        return new ArrayList<>();
+    }
+
+    public static void printKLevelsDown(Node node, int k) {
+        if (node == null) {
+            return;
+        }
+        if (k == 0) {
+            System.out.print(node.data + " ");
+            return;
+        }
+        printKLevelsDown(node.left, k - 1);
+        printKLevelsDown(node.right, k - 1);
+    }
+
     public static void fun() {
         Integer[] arr = { 50, 25, 12, null, null, 37, 30, null, null, null, 75, 62, null, 70, null, null, 87, null,
                 null };
         Node root = construct(arr);
+        int find = 87;
         // display(root);
         // int size = size(root);
         // System.out.println(size);
@@ -164,7 +261,15 @@ public class BinaryTree {
         // int height = height(root);
         // System.out.println(height);
         // travelsal(root);
-        levelOrder(root);
+        // levelOrder(root);
+        // iterativePrePostInTraversal(root);
+        // boolean fin = find(root, find);
+        // System.out.println(fin);
+        // ArrayList<Integer> ans = nodeTopath(root, find);
+        // System.out.println(ans);
+        int k = 1;
+        printKLevelsDown(root, k);
+
     }
 
     public static void main(String[] args) {
