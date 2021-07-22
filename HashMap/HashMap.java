e24bf6be98f6e24421c21cddb84b56bed4ae9a3c
shikhasingh07 <shikha.thakur2295@gmail.com>
2021-07-22 18:42:17 +0530

    update

--- HashMap/HashMap.java
+++ HashMap/HashMap.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class HashMap {
  
   //pepcoding 
   public static void longestConsecutiveSquence(int[] arr , HashMap<Integer , Boolean> has ) {
